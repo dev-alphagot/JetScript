c77962aa381ebd10e1b130b6119c4c0ac132aca0
Minecart8925 <minecart8926@daum.net>
2019-10-30 02:49:29 +0900

    Some Edit...

--- runner/exec.js
+++ runner/exec.js
@@ -1,23 +1,23 @@
 module.exports.run = async (code) => {
     //console.log("Startd!");
     try{
-    let excute_before = code;
+    let execute_before = code;
     let codelet = code.length;
     let code_prograss = 0;
     let virtual_memory = [];
 
     while(code_prograss < codelet){
-        let excute = excute_before[code_prograss].replace("\n","").split("  ");
-        switch(excute[0]){
+        let execute = execute_before[code_prograss].replace("\n","").split("  ");
+        switch(execute[0]){
             case "fnc":
                 //console.log("fnc call");
-                virtual_memory[`${excute[1]}`] = "";
+                virtual_memory[`${execute[1]}`] = "";
                 code_prograss++;
                 break;
             
             case "ioout":
                 //console.log("ioout call");
-                let fnc_print_data = virtual_memory[`${excute[1]}`];
+                let fnc_print_data = virtual_memory[`${execute[1]}`];
                 console.log(fnc_print_data);
                 delete fnc_print_data;
                 code_prograss++;
@@ -29,21 +29,21 @@ module.exports.run = async (code) => {
             
             case "compute":
                 //console.log("compute call");
-                let compute_data = virtual_memory[`${excute[1]}`];
+                let compute_data = virtual_memory[`${execute[1]}`];
                 let compute_result = eval(compute_data);
-                virtual_memory[`${excute[1]}`] = compute_result;
+                virtual_memory[`${execute[1]}`] = compute_result;
                 code_prograss++;
                 delete compute_data;
                 delete compute_result;
                 break;
 
             case "delete":
-                virtual_memory[`${excute[1]}`] = "";
+                virtual_memory[`${execute[1]}`] = "";
                 code_prograss++;
                 break;
 
             case "jump":
-                code_prograss = excute[1];
+                code_prograss = execute[1];
                 code_prograss++;
                 break;
 
@@ -52,11 +52,11 @@ module.exports.run = async (code) => {
                 break;
 
             default:
-                if(excute[1]){
-                    if(excute[1] == "<-"){
-                        virtual_memory[`${excute[0]}`] = excute[2];
-                    } else if (excute[1] == "<="){
-                        virtual_memory[`${excute[0]}`] = virtual_memory[`${excute[2]}`];
+                if(execute[1]){
+                    if(execute[1] == "<-"){
+                        virtual_memory[`${execute[0]}`] = execute[2];
+                    } else if (execute[1] == "<="){
+                        virtual_memory[`${execute[0]}`] = virtual_memory[`${execute[2]}`];
                     }
                 }
                 code_prograss++;
